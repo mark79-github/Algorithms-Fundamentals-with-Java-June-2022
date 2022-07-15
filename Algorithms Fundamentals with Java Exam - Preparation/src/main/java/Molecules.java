8b806302692b2a2d7f43b35592775682c859b4c3
mark79-github <52877024+mark79-github@users.noreply.github.com>
2022-07-15 16:42:16 +0300

    refactoring #Molecules - using HashMap for graph structure;

--- Algorithms Fundamentals with Java Exam - Preparation/src/main/java/Molecules.java
+++ Algorithms Fundamentals with Java Exam - Preparation/src/main/java/Molecules.java
@@ -1,37 +1,32 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Molecules {
-    private static final List<List<Integer>> graph = new ArrayList<>();
-    private static boolean[] isVisited;
+    private static final Map<Integer, Set<Integer>> graph = new HashMap<>();
+    private static Set<Integer> isVisited;
     private static int n;
 
-    private static String generateResult(Deque<Integer> deque) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            if (!deque.contains(i)) {
-                builder.append(i).append(" ");
-            }
-        }
-        return builder.toString();
+    private static void printResult() {
+        String result = IntStream.rangeClosed(1, n).filter(i -> !isVisited.contains(i))
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+        System.out.println(result);
     }
 
-    private static Deque<Integer> dfsWithoutRecursion(int start) {
-        Deque<Integer> elements = new ArrayDeque<>();
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(start);
-        while (!stack.isEmpty()) {
-            int current = stack.pop();
-            if (!isVisited[current]) {
-                isVisited[current] = true;
-                elements.push(current);
-                for (Integer dest : graph.get(current)) {
-                    if (!isVisited[dest]) {
-                        stack.push(dest);
-                    }
+    private static void bfs(int start) {
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        queue.offer(start);
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            isVisited.add(node);
+            for (int child : graph.get(node)) {
+                if (!isVisited.contains(child)) {
+                    queue.offer(child);
                 }
             }
         }
-        return elements;
     }
 
     public static void main(String[] args) {
@@ -40,24 +35,22 @@ public class Molecules {
         n = Integer.parseInt(scanner.nextLine());
         int m = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+        for (int i = 1; i <= n; i++) {
+            graph.putIfAbsent(i, new HashSet<>());
         }
 
-        isVisited = new boolean[n + 1];
+        isVisited = new HashSet<>();
 
         for (int i = 0; i < m; i++) {
             int[] tokens = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
             int from = tokens[0];
             int to = tokens[1];
-
             graph.get(from).add(to);
         }
 
         int start = Integer.parseInt(scanner.nextLine());
 
-        Deque<Integer> deque = dfsWithoutRecursion(start);
-
-        System.out.println(generateResult(deque));
+        bfs(start);
+        printResult();
     }
 }
